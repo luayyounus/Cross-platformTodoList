f32b463096807c423953b0114710b56d3c434e1a
Luay Younus <luayghazwan@yahoo.com>
2017-05-09 17:51:54 -0700

    Add Todo model

---
+++ Cross-platformTodoList/Todo.h
@@ -0,0 +1,16 @@
+//
+//  Todo.h
+//  Cross-platformTodoList
+//
+//  Created by Luay Younus on 5/9/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Todo : NSObject
+
+@property(strong,nonatomic) NSString *title;
+@property(strong,nonatomic) NSString *content;
+
+@end
