e776861bee0f5893a4e3f60e0d22e65c9da7ae3a
Luay Younus <luayghazwan@yahoo.com>
2017-05-09 20:10:43 -0700

    Add TodoRowController for Todo labels

---
+++ WatchKit-TodoList Extension/TodoRowController.h
@@ -0,0 +1,17 @@
+//
+//  TodoRowController.h
+//  Cross-platformTodoList
+//
+//  Created by Luay Younus on 5/9/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import WatchKit;
+
+@interface TodoRowController : NSObject
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *titleLabel;
+
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *contentLabel;
+
+@end
